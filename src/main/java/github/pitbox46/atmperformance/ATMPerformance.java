c607cf786433e517d807e95ba8eba7ab60085bcd
pitbox46 <pitbox46@gmail.com>
2024-02-23 22:56:12 -0500

    Remove require on both sides

--- src/main/java/github/pitbox46/atmperformance/ATMPerformance.java
+++ src/main/java/github/pitbox46/atmperformance/ATMPerformance.java
@@ -1,7 +1,10 @@
 package github.pitbox46.atmperformance;
 
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -9,4 +12,8 @@ import org.slf4j.Logger;
 public class ATMPerformance {
     public static final String MODID = "atmperformance";
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ATMPerformance() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
 }
