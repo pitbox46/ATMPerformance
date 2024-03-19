56ed64e2b407b119d9773478655d9383ea83f6b1
pitbox46 <pitbox46@gmail.com>
2024-03-19 15:14:44 -0400

    Develop benchmarking tool

---
+++ src/main/java/github/pitbox46/atmperformance/benchmark/Benchmark.java
@@ -0,0 +1,83 @@
+package github.pitbox46.atmperformance.benchmark;
+
+import net.minecraft.client.Minecraft;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.function.Supplier;
+
+public class Benchmark {
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String[] HEADER = {"start", "finish"};
+    private final File log;
+
+    public Benchmark(String name) {
+        this.log = createLogFile(name);
+        addHeader();
+    }
+
+    public static <T> Data<T> benchmark(Supplier<T> target) {
+        long start = System.nanoTime();
+        T value = target.get();
+        long finish = System.nanoTime();
+        return new Data<>(value, new String[]{String.valueOf(start), String.valueOf(finish)});
+    }
+
+    public <T> Data<T> benchmarkAndLog(Supplier<T> target) {
+        Data<T> data = benchmark(target);
+        StringBuilder builder = new StringBuilder("\n");
+        for (String value : data.data()) {
+            builder.append(value).append(',');
+        }
+        try (FileWriter writer = new FileWriter(log, true)) {
+            writer.write(builder.toString());
+        } catch (IOException e) {
+            LOGGER.warn(e);
+        }
+        return data;
+    }
+
+    private void addHeader() {
+        StringBuilder builder = new StringBuilder();
+        for (String f : HEADER) {
+            builder.append(f).append(',');
+        }
+        try (FileWriter writer = new FileWriter(log, true)) {
+            writer.write(builder.toString());
+        } catch (IOException e) {
+            LOGGER.warn(e);
+        }
+    }
+
+    private static File createLogFile(String name) {
+        try {
+            File logDirFile = new File(Minecraft.getInstance().gameDirectory.getAbsolutePath(), "logs/atmperformance");
+            Path logDirPath = logDirFile.toPath();
+            if (!logDirFile.isDirectory()) {
+                Files.createDirectory(logDirPath);
+            }
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+            Path logPath = logDirPath.resolve(String.format("%s-%s.csv", name, dateFormat.format(new Date())));
+            File logFile = logPath.toFile();
+            if (!logFile.exists()) {
+                Files.createFile(logPath);
+            }
+            return logFile;
+        } catch (IOException e) {
+            LOGGER.warn(e);
+            return null;
+        }
+    }
+
+    public record Data<T> (T value, String[] data) {}
+}
--- src/main/java/github/pitbox46/atmperformance/mixin/gregtech/ChemicalHelperMixin.java
+++ src/main/java/github/pitbox46/atmperformance/mixin/gregtech/ChemicalHelperMixin.java
@@ -12,6 +12,7 @@ import javax.annotation.Nullable;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 @Mixin(value = ChemicalHelper.class, remap = false)
@@ -20,7 +21,6 @@ public class ChemicalHelperMixin {
     @Shadow @Final public static Map<ItemLike, UnificationEntry> ITEM_UNIFICATION_ENTRY_COLLECTED;
     @Unique
     private static final Object2ObjectMap<ItemLike, UnificationEntry> FAST_ITEM_UNIFICATION_ENTRY_COLLECTED = new Object2ObjectOpenHashMap<>();
-    private static long totalTimeSpent = 0;
 
     /**
      * @author pitbox46
@@ -29,39 +29,31 @@ public class ChemicalHelperMixin {
     @Nullable
     @Overwrite
     public static UnificationEntry getUnificationEntry(ItemLike itemLike) {
-        long start = System.nanoTime();
-
-        for (Map.Entry<Supplier<? extends ItemLike>, UnificationEntry> entry : ITEM_UNIFICATION_ENTRY) {
-            FAST_ITEM_UNIFICATION_ENTRY_COLLECTED.put(entry.getKey().get(), entry.getValue());
-        }
-        ITEM_UNIFICATION_ENTRY.clear();
-
-        UnificationEntry retValue = FAST_ITEM_UNIFICATION_ENTRY_COLLECTED.get(itemLike);
+//        return ATMPerformance.MAIN_BENCHMARK.benchmarkAndLog(() -> {
+//            for (Map.Entry<Supplier<? extends ItemLike>, UnificationEntry> entry : ITEM_UNIFICATION_ENTRY) {
+//                FAST_ITEM_UNIFICATION_ENTRY_COLLECTED.put(entry.getKey().get(), entry.getValue());
+//            }
+//            ITEM_UNIFICATION_ENTRY.clear();
+//
+//            return FAST_ITEM_UNIFICATION_ENTRY_COLLECTED.get(itemLike);
+//        }).value();
 
         // Default Method, here for performance before/after comparison
-//        UnificationEntry retValue = ITEM_UNIFICATION_ENTRY_COLLECTED.computeIfAbsent(itemLike, (item) -> {
-//            Iterator<Map.Entry<Supplier<? extends ItemLike>, UnificationEntry>> var2 = ITEM_UNIFICATION_ENTRY.iterator();
-//
-//            Map.Entry<Supplier<? extends ItemLike>, UnificationEntry> entry;
-//            do {
-//                if (!var2.hasNext()) {
-//                    return null;
-//                }
-//
-//                entry = var2.next();
-//            } while(entry.getKey().get().asItem() != itemLike.asItem());
-//
-//            return entry.getValue();
-//        });
+        return ATMPerformance.MAIN_BENCHMARK.benchmarkAndLog(() -> {
+            return ITEM_UNIFICATION_ENTRY_COLLECTED.computeIfAbsent(itemLike, (item) -> {
+                Iterator<Map.Entry<Supplier<? extends ItemLike>, UnificationEntry>> var2 = ITEM_UNIFICATION_ENTRY.iterator();
 
-        // Debugging and Profiling Code
-        // TODO Implement real performance profiler
-//        long diff = System.nanoTime() - start;
-//        totalTimeSpent += diff;
-//        if (System.nanoTime() % 1000 == 0) {
-//            ATMPerformance.LOGGER.info("Unification - Total Time (ms): {}, Individual Time (ms): {}", totalTimeSpent / 1000000F, diff / 1000000F);
-//        }
+                Map.Entry<Supplier<? extends ItemLike>, UnificationEntry> entry;
+                do {
+                    if (!var2.hasNext()) {
+                        return null;
+                    }
 
-        return retValue;
+                    entry = var2.next();
+                } while(entry.getKey().get().asItem() != itemLike.asItem());
+
+                return entry.getValue();
+            });
+        }).value();
     }
 }
